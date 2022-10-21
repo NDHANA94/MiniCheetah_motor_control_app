614cfeaa66ad4c1e00b3a62a658fefb7560d0adf
NDHANA94 <nipun.dhananjaya@gmail.com>
2022-10-21 16:31:02 +0300

    changedmotor driver baudrate back to 921600

--- Codes/MiniCheetaMotor_driver/motorcontrol-master/Core/Src/main.c
+++ Codes/MiniCheetaMotor_driver/motorcontrol-master/Core/Src/main.c
@@ -169,7 +169,7 @@ int main(void)
   if(isnan(I_MAX_CONT) || I_MAX_CONT==-1){I_MAX_CONT = 14.0f;}
   if(isnan(I_CAL)||I_CAL==-1){I_CAL = 5.0f;}
   if(isnan(PPAIRS) || PPAIRS==-1){PPAIRS = 21.0f;}
-  if(isnan(GR) || GR==-1){GR = 1.0f;}
+  if(isnan(GR) || GR==-1){GR = 6.0f;}
   if(isnan(KT) || KT==-1){KT = 1.0f;}
   if(isnan(KP_MAX) || KP_MAX==-1){KP_MAX = 500.0f;}
   if(isnan(KD_MAX) || KD_MAX==-1){KD_MAX = 5.0f;}
--- Codes/MiniCheetaMotor_driver/motorcontrol-master/Core/Src/math_ops.c
+++ Codes/MiniCheetaMotor_driver/motorcontrol-master/Core/Src/math_ops.c
@@ -41,6 +41,20 @@ void limit(float *x, float min, float max){
     *x = fast_fmaxf(fast_fminf(*x, max), min);
     }
 
+int float_to_uint(float x, float x_min, float x_max, int bits){
+    /// Converts a float to an unsigned int, given range and number of bits ///
+    float span = x_max - x_min;
+    float offset = x_min;
+    return (int) ((x-offset)*((float)((1<<bits)-1))/span);
+    }
+    
+    
+float uint_to_float(int x_int, float x_min, float x_max, int bits){
+    /// converts unsigned int to float, given range and number of bits ///
+    float span = x_max - x_min;
+    float offset = x_min;
+    return ((float)x_int)*span/((float)((1<<bits)-1)) + offset;
+    }
 
 
 float sin_lut(float theta){
